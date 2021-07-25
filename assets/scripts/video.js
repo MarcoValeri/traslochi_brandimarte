9be0dbcc1a840a1addd9af04feb63747fc83360f
Marco Valeri <marcovaleri@hotmail.it>
2021-07-25 14:49:51 +0100

    Finish style the home page that now it is responsive for all kind of screens

--- assets/scripts/video.js
+++ assets/scripts/video.js
@@ -1,17 +1,22 @@
-console.log(`Hello JavaScript`);
+window.onload = () => {
+    
 
-const videoPlayer = document.getElementById("video-player");
-const video = videoPlayer.querySelector("#video");
-const playButton = document.getElementById("play_button");
+    const videoPlayer = document.getElementById("video-player");
+    const video = videoPlayer.querySelector("#video");
+    const playButton = document.getElementById("play_button");
 
-playButton.addEventListener("click", (e) => {
+    playButton.addEventListener("click", (e) => {
 
-    if (video.paused == true) {
-        video.play()
-        e.target.textContent = "Pause";
-    } else {
-        video.pause()
-        e.target.textContent = "Play";
-    }
+        if (video.paused == true) {
+            video.play()
+            //e.target.textContent = "";
+            playButton.style.display = "none";
+        } else {
+            video.pause()
+            //e.target.textContent = "";
+        }
 
-})
+    })
+
+}
+
