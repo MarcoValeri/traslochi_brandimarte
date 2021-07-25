86860733428746feef9f4970283d7301b178096e
Marco Valeri <marcovaleri@hotmail.it>
2021-07-25 21:27:03 +0100

    Finish chi-siamo page and added preventivi, servizi and contatti pages

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -12,7 +12,35 @@ class PageController extends AbstractController {
      * @Route("/", name="app_homepage")
      */
     public function homepage() {
-        return $this->render('page/home.html.twig');
+        return $this->render('pages/home.html.twig');
+    }
+
+    /**
+     * @Route("/chi-siamo", name="app_chi_siamo")
+     */
+    public function chiSiamo() {
+        return $this->render('pages/chi-siamo.html.twig');
+    }
+
+    /**
+     * @Route("/preventivi", name="app_preventivi")
+     */
+    public function preventivi() {
+        return $this->render('pages/preventivi.html.twig');
+    }
+
+    /**
+     * @Route("/servizi", name="app_servizi")
+     */
+    public function servizi() {
+        return $this->render('pages/servizi.html.twig');
+    }
+
+    /**
+     * @Route("/contatti", name="app_contatti")
+     */
+    public function contatti() {
+        return $this->render('pages/contatti.html.twig');
     }
 
 }
